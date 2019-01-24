3cd4facd4ede8e870b85ee49bacfdbe17025d799
Phil Jung <seokhyun.phil.jung@gmail.com>
2019-01-24 11:44:41 +1100

    cant work out what the combination should be

--- count-triplet.js
+++ count-triplet.js
@@ -1,5 +1,9 @@
 var str = require('./count-triplets-massive-string.js');
 
+function isInteger (n) {
+    return n % 1 <= 0.000001;
+}
+
 function countTriplets(arr, r) {
     var curr = [], i = 0, counter = 0;
     var hashMap = arr.reduce(function (acc, cv) { 
@@ -10,6 +14,10 @@ function countTriplets(arr, r) {
     console.log(hashMap);
     for (var key of Object.keys(hashMap)) {
         var val = Number(key);
+        if (!isInteger((Math.log(val) / Math.log(r)))) {
+            console.log('not fit', val,(Math.log(val) / Math.log(r)) );
+            continue;
+        }
         for (let v of [val, val * r, val * r * r]) { 
             if (!hashMap[v]) {
                 curr.push(0);
